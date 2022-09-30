7a4b6fca354e5960f567ea3da9de543931c822bf
Michel Maia <programador.michel.maia@gmail.com>
2022-09-30 17:03:43 -0300

    commit

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor() {
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcE1 = document.querySelector("#display");  
         this._dateE1 = document.querySelector("#data");
@@ -40,6 +41,74 @@ class CalcController {
         });
     }
 
+    clearAll(){
+
+        this._operation = [];
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':                
+                break;
+
+            case 'subtracao':                
+                break;
+
+            case 'divisao':                
+                break;
+
+            case 'multiplicacao':                
+                break;
+
+            case 'porcento':                
+                break;
+            
+            case 'igual':                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default: 
+                this.setError();
+                break;
+
+        }
+    }
+
     initButtonsEvents(){
        let buttons = document.querySelectorAll("#buttons > g, #parts > g");  
 
@@ -54,7 +123,9 @@ class CalcController {
       
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, "click drag", e => {   // para add drag(clica e arrasta) foi preciso substituir o addEventListener porque ele não suporta mais de um evento e criado o evento this.addEventListenerAll no lugar do btn.addEventListener
-                console.log(btn.className.baseVal.replace("btn-", ""));
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn();                
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown" , e => {
